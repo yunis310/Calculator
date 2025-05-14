33f4892f8de8fe737cef0a82c491611e42fce80c
yunis310 <yunis310@gmail.com>
2025-05-14 16:57:25 -0500

    add Input

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+'use client';
 import Body from '@/components/Body';
 import Input from '@/components/Input';
 import Result from '@/components/Result';
+import { useState } from 'react';
 
 export default function Home() {
+    const [inputValue, setInputValue] = useState('');
+    const [result, setResult] = useState('');
+
+    const handelInput = (str: string) => {
+        setInputValue(str);
+    };
+    const handleClear = () => {
+        setInputValue('');
+        setResult('');
+    };
+
     return (
-        <div>
-            <h1>hello world</h1>
-            <Input />
+        <div className="flex flex-col items-center p-4 ">
+            <Input value={inputValue} onChange={setInputValue} />
             <Result />
             <Body />
         </div>
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 interface Props {
-    id?: string;
+    value: string;
+    onChange: (value: string) => void;
 }
 
-const Input: React.FC<Props> = () => {
-    return <p>Input</p>;
+const Input: React.FC<Props> = ({ value, onChange }) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let inputValue = e.target.value;
+        inputValue = inputValue.replace(/[^0-9+\-*/.]/g, '');
+        inputValue = inputValue.replace(/([+\-*/.]){2,}/g, '$1');
+        inputValue = inputValue.replace(/^[+*/.]/, '');
+
+        onChange(inputValue);
+    };
+
+    return (
+        <input
+            type="text"
+            value={value}
+            onChange={handleChange}
+            placeholder=""
+            className="border-2 p-1"
+        />
+    );
 };
 
 export default Input;
